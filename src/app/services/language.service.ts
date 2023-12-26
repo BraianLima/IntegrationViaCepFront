16b36bcd9420a9e215a5137861d5e936c09e1789
Braian Freitas <braianfreitasdelima@gmail.com>
2023-12-26 17:29:12 -0300

    translate service

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID, Renderer2, RendererFactory2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DOCUMENT, isPlatformBrowser  } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -7,22 +8,32 @@ import { TranslateService } from '@ngx-translate/core';
 export class LanguageService {
   private browserLanguage: string = navigator.language;
 
-  constructor (private translateService: TranslateService){ }
+  constructor (private translateService: TranslateService, private rendererFactory: RendererFactory2,
+    @Inject(DOCUMENT) private document: Document,  @Inject(PLATFORM_ID) private platformId: any){ }
 
   public setUseLanguage() {
     const userLang = navigator.language || "en";
-    const languageCode = userLang.split('-')[0];
-    this.translateService.setDefaultLang(languageCode);
-    this.translateService.use(languageCode);
-    this.setBrowserLanguage(languageCode);
+    this.browserLanguage = userLang.split('-')[0];
+    this.translateService.setDefaultLang(this.browserLanguage);
+    this.translateService.use(this.browserLanguage);
+    this.setBrowserLanguage(this.browserLanguage);
+    if (isPlatformBrowser(this.platformId)) {
+      this.setHtmlLangAttribute();
+    }
   }
   
-  private setBrowserLanguage(languageCode: string) {
-    this.browserLanguage = languageCode;
+  private setBrowserLanguage(browserLanguage: string) {
+    this.browserLanguage = browserLanguage;
   }
   
   private getBrowserLanguage(): string {
     return this.browserLanguage;
+  }
+
+  private setHtmlLangAttribute() {
+    const renderer = this.rendererFactory.createRenderer(null, null);
+    const htmlElement = this.document.documentElement;
+    renderer.setAttribute(htmlElement, 'lang', this.browserLanguage);
   }
 
 }
