9a7328b302758c5ea577dc1c4408d8504fc0f271
Braian Freitas <braianfreitasdelima@gmail.com>
2023-12-26 17:55:44 -0300

    save user language in cookie

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,30 +1,51 @@
 import { Inject, Injectable, RendererFactory2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { DOCUMENT } from '@angular/common';
+import { CookieService } from 'ngx-cookie-service';
+import { isNullOrEmpty } from '../utils/string-utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
   private browserLanguage: string = navigator.language;
+  private cookieName = 'language';
+  private cookieExpirationHours: number = 1; 
 
   constructor (private translateService: TranslateService, private rendererFactory: RendererFactory2,
+    private cookieService: CookieService,
     @Inject(DOCUMENT) private document: Document){ }
 
   public setUseLanguage() {
-    const userLang = navigator.language || "en";
-    this.browserLanguage = userLang.split('-')[0];
+    this.browserLanguage = this.getBrowserLanguage();
     this.translateService.setDefaultLang(this.browserLanguage);
     this.translateService.use(this.browserLanguage);
     this.setHtmlLangAttribute();
-  }
-  
-  private setBrowserLanguage(browserLanguage: string) {
-    this.browserLanguage = browserLanguage;
+    if (!this.existsLanguageCookie()){
+      this.setCookieLanguage(this.browserLanguage);
+    }
   }
   
   private getBrowserLanguage(): string {
-    return this.browserLanguage;
+    const storedLanguage = this.cookieService.get(this.cookieName);
+    if (!isNullOrEmpty(storedLanguage)){
+      return storedLanguage;
+    }
+
+    const userLang = navigator.language || 'en';
+    const browserLanguage = userLang.split('-')[0]
+    return browserLanguage;
+  }
+
+  private setCookieLanguage(browserLanguage: string) {
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + this.cookieExpirationHours * 60 * 60 * 1000);
+    this.cookieService.set(this.cookieName, browserLanguage, expirationDate, '/');
+  }
+
+  private existsLanguageCookie(): boolean {
+    const storedLanguage = this.cookieService.get(this.cookieName);
+    return !isNullOrEmpty(storedLanguage);
   }
 
   private setHtmlLangAttribute() {
