d5b1cd5a84ed28ab48929b7c521cc79950d22d4a
Braian Freitas <braianfreitasdelima@gmail.com>
2024-01-05 15:53:43 -0300

    getPostalCode and searchPostalCode works

--- src/app/components/content/cep-output/cep-output.component.ts
+++ src/app/components/content/cep-output/cep-output.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { responseGetCEP } from '../../../models/responseGetPostalCode';
 import { ClipboardService } from '../../../services/clipboard.service';
 import { responseSearchPostalCode } from '../../../models/responseSearchPostalCode';
@@ -9,15 +9,42 @@ import { postalCode } from '../../../models/postalCode';
   templateUrl: './cep-output.component.html',
   styleUrl: './cep-output.component.css'
 })
-export class CepOutputComponent {
+export class CepOutputComponent implements OnInit, OnChanges  {
   @Input() address: responseGetCEP = new responseGetCEP;
   @Input() isBlockSearchPostalCode: boolean = false; 
   @Input() postalCode: postalCode = new postalCode;
+  public postalCodeClipboard: postalCode = new postalCode;
   
   constructor(private  clipboardService: ClipboardService) {}
+
+  ngOnInit(): void {
+    this.initializePostalCodeClipboard();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['address']) {
+      this.initializePostalCodeClipboard();
+    }
+  }
+  
+  private initializePostalCodeClipboard(): void {
+    if (this.isBlockSearchPostalCode) {
+      this.postalCodeClipboard = { ...this.postalCode };
+      return;
+    }
+    
+    if (!this.address.data) {
+      this.address.data = new postalCode;
+    }
+    this.postalCodeClipboard = { ...this.address.data };
+  }
 
   public copyToClipboardByService(inputId: string, event: Event): void {
     const buttonElement = event.currentTarget as HTMLButtonElement;
     this.clipboardService.copyToClipboard(inputId, buttonElement);
+  }
+
+  public copyAllAddressToClipboardByService(): void{
+    console.log(this.postalCodeClipboard)
   }
 }
